229fd5a3ecca9c09c64488f30af6d5e5e8a9d1ca
patrickatcw <pj8392@yahoo.com>
2017-08-10 21:44:08 -0400

    working on timer

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,27 +1,7 @@
-    /*var number = 20;
-    var intervalId;
-
-    $("#stop").on("click", stop);
-     $("#resume").on("click", run);
-
-    function run() {
-      intervalId = setInterval(decrement, 1000);
-                  }
-
-    function decrement() {
-        number--;
-
-      $("#countdown").html("<h1>" + number + "</h1>");
-
-       if (number === 0) {
-
-        stop();
-
-        alert("Time Up!");
-      }
-    }
-
-    function stop() {
-    clearInterval(intervalId);
-    }
-    run();*/
+    /*var timeleft = 20;
+    var downloadTimer = setInterval(function(){
+    timeleft--;
+    document.getElementById("countdowntimer").textContent = timeleft;
+    if(timeleft <= 0)
+        clearInterval(downloadTimer);
+    },1000);*/
