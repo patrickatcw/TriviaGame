de6dcfd31d561157f2fb67ce7923889f4cd78d43
patrickatcw <pj8392@yahoo.com>
2017-08-10 20:18:45 -0400

    lots of initial work started

---
+++ assets/javascript/app.js
@@ -0,0 +1,27 @@
+    /*var number = 20;
+    var intervalId;
+
+    $("#stop").on("click", stop);
+     $("#resume").on("click", run);
+
+    function run() {
+      intervalId = setInterval(decrement, 1000);
+                  }
+
+    function decrement() {
+        number--;
+
+      $("#countdown").html("<h1>" + number + "</h1>");
+
+       if (number === 0) {
+
+        stop();
+
+        alert("Time Up!");
+      }
+    }
+
+    function stop() {
+    clearInterval(intervalId);
+    }
+    run();*/
