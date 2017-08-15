4da6c4ec355537393057cf90ad4b541a11ce406d
patrickatcw <pj8392@yahoo.com>
2017-08-15 10:45:58 -0400

    starting to link pages through jQuery

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,7 +1,77 @@
-    var timeleft = 20;
+   
+  $(document).ready(function()
+  {
+
+  $("#popcorn").on('click', function()
+
+    {$(".firstpage").hide()
+
+  $(".timer").show() 
+  $(".question1").show()
+    });
+
+
+     var timeleft = 20;
     var downloadTimer = setInterval(function(){
     timeleft--;
     document.getElementById("countdowntimer").textContent = timeleft;
     if(timeleft <= 0)
         clearInterval(downloadTimer);
     },1000);
+
+    })
+
+ 
+
+
+
+
+    /*from Luke
+
+    $('.start').on('click', function()
+    { 
+        progressBar.css('width', '0%')
+        $('.initial').hide()
+        $('.how-you-did').hide()
+        $('.game-board').show()
+        $('.start').hide()
+        run(questionBank[0])
+    })*/
+    
+
+
+    /*var popCorn
+
+
+    $("#popCorn").on("click", function() 
+    {executeTurn(popcorn);
+    });*/
+
+
+   /* $('#popCorn').on('click',function(){
+    if($('#header', '#directions', '#button', '#aside').css('display')!='none'){
+    $('#timer', '#question1').show().siblings('div').hide();
+    }else if($('#timer', '#question1').css('display')!='none'){
+        $('#header', '#directions', '#button', '#aside').show().siblings('div').hide();
+    }
+    });*/
+
+
+                    //or
+
+
+    /*$(function(){
+         $('#showall').click(function(){
+               $('.targetDiv').show();
+        });
+        $('.showSingle').click(function(){
+              $('.targetDiv').hide();
+              $('#div'+$(this).attr('target')).show();
+        });
+      });*/
+
+
+
+   
+    /*var audio = new Audio("horror.mp3");
+     audio.play();*/
